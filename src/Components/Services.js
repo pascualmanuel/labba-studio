4b66a4d225d3c402461588aba8f5ca72d88db537
pascualmanuel <manuelnpascual@gmail.com>
2023-10-01 15:11:15 +0200

    Services Cursor

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,8 +1,103 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Asset from "../Assets/plus.svg";
+import AssetTwo from "../Assets/mnius.svg"; // Assuming AssetTwo is the minus cursor image
 
 function Services() {
+  const [expDesign, setExpanded] = useState(false);
+
+  const toggleDesign = () => {
+    setExpanded(!expDesign);
+  };
+
+  const [expBuild, setExpBuild] = useState(false);
+
+  const toggleBuild = () => {
+    setExpBuild(!expBuild);
+  };
+
+  useEffect(() => {
+    const plusCursorParagraphs = document.querySelectorAll(".plusCursorDos");
+
+    const handleMouseEnter = () => {
+      // Change cursor behavior when entering the plusCursor
+      // For example, you can change the cursor style or add animations here
+
+      // Change the cursor's background based on the state
+      const cursor = document.querySelector(".cursor");
+      if (cursor) {
+        cursor.style.backgroundImage = `url(${expBuild ? AssetTwo : Asset})`;
+        cursor.style.backgroundSize = "70%";
+        cursor.style.backgroundRepeat = "no-repeat";
+        cursor.style.backgroundPosition = "center";
+      }
+    };
+
+    const handleMouseLeave = () => {
+      // Restore default cursor behavior when leaving the plusCursor
+
+      // Reset the cursor's background image to none
+      const cursor = document.querySelector(".cursor");
+      if (cursor) {
+        cursor.style.backgroundImage = "none";
+      }
+    };
+
+    plusCursorParagraphs.forEach((paragraph) => {
+      paragraph.addEventListener("mouseenter", handleMouseEnter);
+      paragraph.addEventListener("mouseleave", handleMouseLeave);
+    });
+
+    // Clean up event listeners when the component unmounts
+    return () => {
+      plusCursorParagraphs.forEach((paragraph) => {
+        paragraph.removeEventListener("mouseenter", handleMouseEnter);
+        paragraph.removeEventListener("mouseleave", handleMouseLeave);
+      });
+    };
+  }, [expBuild]);
+  useEffect(() => {
+    const plusCursorParagraphs = document.querySelectorAll(".plusCursor");
+
+    const handleMouseEnter = () => {
+      // Change cursor behavior when entering the plusCursor
+      // For example, you can change the cursor style or add animations here
+
+      // Change the cursor's background based on the state
+      const cursor = document.querySelector(".cursor");
+      if (cursor) {
+        cursor.style.backgroundImage = `url(${expDesign ? AssetTwo : Asset})`;
+        cursor.style.backgroundSize = "70%";
+        cursor.style.backgroundRepeat = "no-repeat";
+        cursor.style.backgroundPosition = "center";
+      }
+    };
+
+    const handleMouseLeave = () => {
+      // Restore default cursor behavior when leaving the plusCursor
+
+      // Reset the cursor's background image to none
+      const cursor = document.querySelector(".cursor");
+      if (cursor) {
+        cursor.style.backgroundImage = "none";
+      }
+    };
+
+    // Add event listeners to each <p> element inside the plusCursor
+    plusCursorParagraphs.forEach((paragraph) => {
+      paragraph.addEventListener("mouseenter", handleMouseEnter);
+      paragraph.addEventListener("mouseleave", handleMouseLeave);
+    });
+
+    // Clean up event listeners when the component unmounts
+    return () => {
+      plusCursorParagraphs.forEach((paragraph) => {
+        paragraph.removeEventListener("mouseenter", handleMouseEnter);
+        paragraph.removeEventListener("mouseleave", handleMouseLeave);
+      });
+    };
+  }, [expDesign, expBuild]);
+
   return (
     <>
       <div className="flex mt-40 ml-36 ">
@@ -14,12 +109,48 @@ function Services() {
           </h3>
         </div>
         <div className="w-1/2 flex flex-col pl-72 justify-center ">
-          <p className="sp1-desk flex mb-8 specific-div">
-            DESIGN <span className="small-numb">5</span>
-          </p>
-          <p className="sp1-desk flex ">
-            BUILD <span className="small-numb">4</span>
-          </p>
+          <div
+            className={`w-1/2 flex flex-col pl-72 justify-center plusCursor services  ${
+              expDesign ? "expDesign" : ""
+            }`}
+            onClick={toggleDesign}
+          >
+            <p className="sp1-desk flex mb-2 plusCursor ">
+              DESIGN <span className="small-numb">5</span>
+            </p>
+            {expDesign ? (
+              <>
+                <ul className="list-disc ml-5 plusCursor">
+                  <li>UX Research</li>
+                  <li>UX Design</li>
+                  <li>UI Design</li>
+                  <li>Brand Design</li>
+                  <li>Graphic Design</li>
+                </ul>
+              </>
+            ) : null}
+          </div>
+
+          <div
+            className={`w-1/2 flex flex-col pl-72 justify-center plusCursorDos services  ${
+              expBuild ? "expBuild" : ""
+            }`}
+            onClick={toggleBuild}
+          >
+            <p className="sp1-desk flex mb-2 plusCursorDos ">
+              BUILD <span className="small-numb">4</span>
+            </p>
+            {expBuild ? (
+              <>
+                <ul className="list-disc ml-5 plusCursorDos ">
+                  <li>Full-stack development</li>
+                  <li>Responsive web development</li>
+                  <li>No-code development</li>
+                  <li>App development</li>
+                </ul>
+              </>
+            ) : null}
+          </div>
         </div>
       </div>
     </>
--- src/Hooks/CustomCursor.js
+++ src/Hooks/CustomCursor.js
@@ -1,9 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { gsap } from "gsap";
 
 const CustomCursor = () => {
   const [isMoving, setIsMoving] = useState(false);
   const [cursorScaleElements, setCursorScaleElements] = useState([]);
+  const [isInsideHello, setIsInsideHello] = useState(false);
+
+  const cursorStyle = {
+    zIndex: 9999,
+    position: "fixed",
+    width: "34px",
+    height: "34px",
+    border: "1px solid #000000",
+    borderRadius: isInsideHello ? "10px" : "100px",
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
+    pointerEvents: "none",
+    transition: "transform 0.3s ease-out, border-radius 0.3s ease-out",
+    backdropFilter: "blur(2px)",
+  };
+
+  const plusCursorRef = useRef(null);
 
   useEffect(() => {
     const cursor = document.querySelector(".cursor");
@@ -34,17 +50,42 @@ const CustomCursor = () => {
       setIsMoving(false);
     };
 
+    const plusCursor = plusCursorRef.current;
+
+    if (plusCursor) {
+      plusCursor.addEventListener("mouseenter", () => {
+        setIsInsideHello(true);
+      });
+
+      plusCursor.addEventListener("mouseleave", () => {
+        setIsInsideHello(false);
+      });
+    }
+
     window.addEventListener("mousemove", handleMouseMove);
     window.addEventListener("mouseleave", handleMouseLeave);
 
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
       window.removeEventListener("mouseleave", handleMouseLeave);
+
+      if (plusCursor) {
+        plusCursor.removeEventListener("mouseenter", () => {
+          setIsInsideHello(true);
+        });
+
+        plusCursor.removeEventListener("mouseleave", () => {
+          setIsInsideHello(false);
+        });
+      }
     };
-  }, []);
+  }, [plusCursorRef]);
 
   return (
-    <div className={`cursor ${isMoving ? "is-moving" : ""}`}>
+    <div
+      className={`cursor ${isMoving ? "is-moving" : ""}`}
+      style={cursorStyle}
+    >
       {cursorScaleElements.map((link, index) => (
         <div
           key={index}
