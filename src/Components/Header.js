db324076a9f42a107a24761401c7fcdf747a7a69
pascualmanuel <manuelnpascual@gmail.com>
2023-10-05 20:46:36 +0200

    Contact quasi done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,105 +3,136 @@ import LabbaISO from "../Assets/svg-iso-labba.svg";
 import LabbaLogo from "../Assets/svg-labba.svg";
 import Burger from "../Assets/Burger.svg";
 import BurgerClose from "../Assets/Burger-close.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import BackIcon from "../Assets/Back.svg";
 import { useState } from "react";
 function Header() {
   const [expanded, setExpanded] = useState(false);
+  const location = useLocation();
 
   const toggleContact = () => {
     setExpanded(!expanded);
   };
 
-  return (
-    <>
-      <div className="flex flex-row justify-between items-center h-24">
-        <Link to="/">
-          <div className="">
-            <ReactSVG
-              src={LabbaISO}
-              className="mr-2.5 ml-12 fixed"
-              style={{ zIndex: "10000" }}
-            />
-            <ReactSVG
-              src={LabbaLogo}
-              className="ml-12 ml-[80px]"
-              style={{ zIndex: "10000" }}
-            />
-          </div>
-        </Link>
-        <div className="fixed top-6 right-10" style={{ zIndex: "100" }}>
-          <div className={`contact ${expanded ? "expanded" : ""}`}>
-            {expanded ? (
-              <div className="expanded-content w-full h-full">
-                <div
-                  style={{
-                    marginTop: "15px",
-                    marginLeft: "28px",
-                    marginBottom: "44px",
-                  }}
-                >
-                  <span className="b3-desk mr-4" style={{ fontSize: "16px" }}>
-                    EN
-                  </span>
-                  <span className="b3-desk" style={{ fontSize: "16px" }}>
-                    ES
-                  </span>
-                </div>
-                <div
-                  style={{
-                    marginLeft: "28px",
-                  }}
-                >
-                  <p
-                    className="b1-desk"
-                    style={{ fontSize: "24px", marginBottom: "10px" }}
+  if (location.pathname === "/") {
+    return (
+      <>
+        <div className="flex flex-row justify-between items-center h-24">
+          <Link to="/">
+            <div className="">
+              <ReactSVG
+                src={LabbaISO}
+                className="mr-2.5 ml-12 fixed"
+                style={{ zIndex: "10000" }}
+              />
+              <ReactSVG
+                src={LabbaLogo}
+                className="ml-12 ml-[80px]"
+                style={{ zIndex: "10000" }}
+              />
+            </div>
+          </Link>
+          <div className="fixed top-6 right-10" style={{ zIndex: "100" }}>
+            <div className={`contact ${expanded ? "expanded" : ""}`}>
+              {expanded ? (
+                <div className="expanded-content w-full h-full">
+                  <div
+                    style={{
+                      marginTop: "15px",
+                      marginLeft: "28px",
+                      marginBottom: "44px",
+                    }}
                   >
-                    Work
-                  </p>
-                  <p
-                    className="b1-desk"
-                    style={{ fontSize: "24px", marginBottom: "10px" }}
+                    <span className="b3-desk mr-4" style={{ fontSize: "16px" }}>
+                      EN
+                    </span>
+                    <span className="b3-desk" style={{ fontSize: "16px" }}>
+                      ES
+                    </span>
+                  </div>
+                  <div
+                    style={{
+                      marginLeft: "28px",
+                    }}
                   >
-                    Services
-                  </p>
-                  <p
-                    className="b1-desk"
-                    style={{ fontSize: "24px", marginBottom: "10px" }}
-                  >
-                    Process
-                  </p>
-                  <Link to="/contact">
                     <p
                       className="b1-desk"
                       style={{ fontSize: "24px", marginBottom: "10px" }}
                     >
-                      Contact
+                      Work
                     </p>
-                  </Link>
+                    <p
+                      className="b1-desk"
+                      style={{ fontSize: "24px", marginBottom: "10px" }}
+                    >
+                      Services
+                    </p>
+                    <p
+                      className="b1-desk"
+                      style={{ fontSize: "24px", marginBottom: "10px" }}
+                    >
+                      Process
+                    </p>
+                    <Link to="/contact">
+                      <p
+                        className="b1-desk"
+                        style={{ fontSize: "24px", marginBottom: "10px" }}
+                      >
+                        Contact
+                      </p>
+                    </Link>
+                  </div>
                 </div>
+              ) : null}
+              <div className="menu-icon">
+                {expanded ? null : (
+                  <Link to="/contact">
+                    <p className="b3-desk pl-2.5"> Contact </p>
+                  </Link>
+                )}
+                <ReactSVG
+                  src={expanded ? BurgerClose : Burger}
+                  style={{
+                    position: "fixed",
+                    top: "4px",
+                    right: "10px",
+                    zIndex: "101",
+                  }}
+                  onClick={toggleContact}
+                />
               </div>
-            ) : null}
-            <div className="menu-icon">
-              {expanded ? null : (
-                <Link to="/contact">
-                  <p className="b3-desk pl-2.5"> Contact </p>
-                </Link>
-              )}
-              <ReactSVG
-                src={expanded ? BurgerClose : Burger}
-                style={{
-                  position: "fixed",
-                  top: "4px",
-                  right: "10px",
-                  zIndex: "101",
-                }}
-                onClick={toggleContact}
-              />
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="flex flex-row justify-between items-center h-24">
+          <Link to="/">
+            <div className="">
+              <ReactSVG
+                src={LabbaISO}
+                className="mr-2.5 ml-12 fixed"
+                style={{ zIndex: "10000" }}
+              />
+              <ReactSVG
+                src={LabbaLogo}
+                className="ml-12 ml-[80px]"
+                style={{ zIndex: "10000" }}
+              />
+            </div>
+          </Link>
+          <Link to="/">
+            <div className="contact-page">
+              <ReactSVG src={BackIcon} onClick={toggleContact} />
+              <p className="b3-desk pl-2.5"> Back </p>
+            </div>
+          </Link>
+        </div>
+      </>
+    );
+  }
 }
 export default Header;
