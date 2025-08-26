db718d67d0801e799f450a562779bd45014a799a
pascualmanuel <manuelnpascual@gmail.com>
2025-08-26 14:05:40 +0200

    Header y home hero

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,6 +15,9 @@ import MagneticButton from "./MagenticButton";
 function Header() {
   const [isNavbarVisible, setIsNavbarVisible] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  console.log(isMenuOpen);
+
   useEffect(() => {
     const onScroll = () => {
       const y = window.scrollY;
@@ -101,68 +104,231 @@ function Header() {
   // transition-transform duration-300 will-change-transform
   // ${isNavbarVisible ? "translate-y-0" : "-translate-y-full"}`}
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      setIsVisible(true);
+    } else {
+      const timeout = setTimeout(() => {
+        setIsVisible(false);
+      }, 600);
+      return () => clearTimeout(timeout);
+    }
+  }, [isMenuOpen]);
+
   if (location.pathname != "/contact") {
     return (
       <>
         <div
-          className={`z-[100] fixed w-screen center flex flex-row justify-between items-center 
-        h-[77px] max-w-[1500px] sm:h-32 px-[15px] sm:px-[50px] pointer-events-none transition-transform duration-300 will-change-transform
+          className={`z-[100] fixed w-full center flex flex-row justify-between items-center 
+        h-[77px]  sm:h-32   transition-transform duration-300 will-change-transform
         ${isNavbarVisible ? "translate-y-0" : "-translate-y-full"}
          ${isWorkPage ? "" : "fixed"} `}
         >
-          <Link to="/#home">
-            <div
-              className=" pointer-events-auto"
-              style={{
-                zIndex: "10006",
-              }}
-            >
-              <ReactSVG
-                src={isWorkPage ? LabbaWhiteLogo : LabbaLogo}
-                className=" "
-                style={{ zIndex: "10006" }}
-              />
-            </div>
-          </Link>
+          <div
+            className="
+     
+    flex items-center justify-between mx-auto max-w-[1500px] w-full px-16
+          
+          "
+          >
+            <Link to="/#home">
+              <div className=" pointer-events-auto">
+                <ReactSVG
+                  src={isWorkPage ? LabbaWhiteLogo : LabbaLogo}
+                  className=" "
+                />
+              </div>
+            </Link>
 
-          <Link to="/contact" className="btn-contact">
-            <span>Get in touch</span>
-            <svg
-              className="btn-border"
-              viewBox="0 0 100 46"
-              preserveAspectRatio="none"
-              aria-hidden="true"
-            >
-              <defs>
-                <linearGradient
-                  id="glass-shimmer"
-                  x1="0%"
-                  y1="0%"
-                  x2="100%"
-                  y2="0%"
+            <div className="flex flex-row gap-4 items-center">
+              <Link className="hidden md:block" to="/services">
+                Services
+              </Link>
+              <Link className="hidden md:block" to="/work">
+                Work
+              </Link>
+              <Link className="hidden md:block" to="/about">
+                About
+              </Link>
+              <Link className="hidden md:block mr-6" to="/blog">
+                Blog
+              </Link>
+
+              <Link to="/contact" className="btn-contact">
+                <span>Get in touch</span>
+                <svg
+                  className="btn-border"
+                  viewBox="0 0 100 46"
+                  preserveAspectRatio="none"
+                  aria-hidden="true"
                 >
-                  <stop offset="0%" stop-color="rgba(255,255,255,0)" />
-                  <stop offset="50%" stop-color="rgba(255,255,255,1)" />
-                  <stop offset="100%" stop-color="rgba(255,255,255,0)" />
-                </linearGradient>
-              </defs>
-              <rect
-                x="1"
-                y="1"
-                width="98"
-                height="38"
-                rx="7"
-                ry="7"
-                pathLength="100"
-                fill="none"
-                stroke="url(#glass-shimmer)"
-                strokeWidth="2px"
-                strokeLinecap="round"
-                strokeDasharray="16 84"
-              />
-            </svg>
-          </Link>
+                  <defs>
+                    <linearGradient
+                      id="glass-shimmer"
+                      x1="0%"
+                      y1="0%"
+                      x2="100%"
+                      y2="0%"
+                    >
+                      <stop offset="0%" stop-color="rgba(255,255,255,0)" />
+                      <stop offset="50%" stop-color="rgba(255,255,255,1)" />
+                      <stop offset="100%" stop-color="rgba(255,255,255,0)" />
+                    </linearGradient>
+                  </defs>
+                  <rect
+                    x="1"
+                    y="1"
+                    width="98"
+                    height="38"
+                    rx="7"
+                    ry="7"
+                    pathLength="100"
+                    fill="none"
+                    stroke="url(#glass-shimmer)"
+                    strokeWidth="2px"
+                    strokeLinecap="round"
+                    strokeDasharray="16 84"
+                  />
+                </svg>
+              </Link>
+              <div className="md:hidden z-999">
+                <button
+                  onClick={toggleMenu}
+                  style={{
+                    background: "rgba(255,255,255,0.1)",
+                    height: 40,
+                    width: 40,
+                  }}
+                  className="inline-flex items-center justify-center p-2 rounded-[999px] relative"
+                  aria-expanded={isMenuOpen}
+                >
+                  <span className="sr-only">Abrir menú principal</span>
+                  <svg
+                    className="h-6 w-6 mt-[2px]"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke={"#F2F2F2"}
+                    aria-hidden="true"
+                  >
+                    <path
+                      className={`hamburger-line top ${
+                        isMenuOpen ? "cross" : ""
+                      }`}
+                      strokeWidth="2"
+                      d="M4 8h16"
+                    />
+                    <path
+                      className={`hamburger-line bottom ${
+                        isMenuOpen ? "cross" : ""
+                      }`}
+                      strokeWidth="2"
+                      d="M4 14h16"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
+        {isVisible && (
+          <div
+            className={`${
+              isMenuOpen
+                ? "opacity-100 transition-opacity duration-600 bg-black/80 "
+                : " transition-opacity duration-600 opacity-0"
+            }  fixed inset-0 z-40`}
+          >
+            <div className="w-full h-full flex flex-col px-4 pb-8">
+              <div className="flex-1 flex flex-col md:flex-row md:items-center md:justify-center justify-end pb-10 ">
+                <div
+                  className={`  md:flex-row ${
+                    isMenuOpen ? "block md:flex" : "hidden"
+                  }`}
+                >
+                  <Link
+                    href="/services"
+                    className="flex items-center vigo text-6xl text-white tracking-[-2%] hover:text-[#c7c7c7] transition-colors duration-200 mb-2"
+                    onClick={closeMenu}
+                  >
+                    Services
+                  </Link>
+                  <Link
+                    href="/work"
+                    className="flex items-center vigo text-6xl text-white tracking-[-2%] hover:text-[#c7c7c7] transition-colors duration-200 mb-2"
+                    onClick={closeMenu}
+                  >
+                    Work
+                  </Link>
+                  <Link
+                    href="/about"
+                    className="flex items-center vigo text-6xl text-white tracking-[-2%] hover:text-[#c7c7c7] transition-colors duration-200 mb-2"
+                    onClick={closeMenu}
+                  >
+                    About
+                  </Link>
+                  <Link
+                    href="/blog"
+                    className="flex items-center vigo text-6xl text-white tracking-[-2%] hover:text-[#c7c7c7] transition-colors duration-200 mb-2"
+                    onClick={closeMenu}
+                  >
+                    Blog
+                  </Link>
+                </div>
+                <Link
+                  to="/contact"
+                  className="btn-contact"
+                  style={{ width: "100%" }}
+                >
+                  <span>Get in touch</span>
+                  <svg
+                    className="btn-border"
+                    viewBox="0 0 100 46"
+                    preserveAspectRatio="none"
+                    aria-hidden="true"
+                  >
+                    <defs>
+                      <linearGradient
+                        id="glass-shimmer"
+                        x1="0%"
+                        y1="0%"
+                        x2="100%"
+                        y2="0%"
+                      >
+                        <stop offset="0%" stop-color="rgba(255,255,255,0)" />
+                        <stop offset="50%" stop-color="rgba(255,255,255,1)" />
+                        <stop offset="100%" stop-color="rgba(255,255,255,0)" />
+                      </linearGradient>
+                    </defs>
+                    <rect
+                      x="1"
+                      y="1"
+                      width="98"
+                      height="38"
+                      rx="7"
+                      ry="7"
+                      pathLength="100"
+                      fill="none"
+                      stroke="url(#glass-shimmer)"
+                      strokeWidth="2px"
+                      strokeLinecap="round"
+                      strokeDasharray="16 84"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          </div>
+        )}
       </>
     );
   } else {
--- src/Components/HomeHero.js
+++ src/Components/HomeHero.js
@@ -7,81 +7,10 @@ import MagneticButton from "./MagenticButton";
 import NewWorks from "../Components/NewWork";
 // import StaggeredText from "../Hooks/StaggeredText";
 const HomeHero = () => {
-  const [isSticky, setIsSticky] = useState(true);
-  const [isStickyTwo, setIsStickyTwo] = useState(true);
-  const [showDelayedElement, setShowDelayedElement] = useState(false);
-  const [showRevealText, setShowRevealText] = useState(false);
-
-  const blackDivRef = useRef(null);
-  const h3Ref = useRef(null);
-  const spanRef = useRef(null);
-
-  const checkOverlap = (element1, element2) => {
-    const rect1 = element1.getBoundingClientRect();
-    const rect2 = element2.getBoundingClientRect();
-
-    return rect1.bottom >= rect2.top;
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const blackDiv = blackDivRef.current;
-      const h3Element = h3Ref.current;
-      const spanElement = spanRef.current;
-
-      if (blackDiv && h3Element && spanElement) {
-        const overlapWithSpan = checkOverlap(blackDiv, spanElement);
-        const overlapWithH3 = checkOverlap(blackDiv, h3Element);
-
-        if (isSticky !== overlapWithSpan) {
-          setIsSticky(overlapWithSpan);
-        }
-        if (isStickyTwo !== overlapWithH3) {
-          setIsStickyTwo(overlapWithH3);
-        }
-      }
-    };
-
-    // Llama a handleScroll al montar el componente
-    handleScroll();
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [isSticky, isStickyTwo]);
-
-  // useEffect(() => {
-  //   if (window.location.hash !== "#home") {
-  //     const delay = setTimeout(() => {
-  //       setShowDelayedElement(true);
-  //       setShowRevealText(true);
-
-  //       const hideDelay = setTimeout(() => {
-  //         setShowRevealText(false);
-  //       }, 2600); // Ocultar después de 1500 ms
-
-  //       return () => clearTimeout(hideDelay);
-  //     }, 4400); // Mostrar después de 4400 ms
-
-  //     return () => clearTimeout(delay);
-  //   } else {
-  //     const delay = setTimeout(() => {
-  //       setShowDelayedElement(true);
-  //       setShowRevealText(true);
-
-  //       const hideDelay = setTimeout(() => {
-  //         setShowRevealText(false);
-  //       }, 1600); // Ocultar después de 1500 ms
-
-  //       return () => clearTimeout(hideDelay);
-  //     }, 200); // Mostrar después de 4400 ms
-
-  //     return () => clearTimeout(delay);
-  //   }
-  // }, []);
-
+  // margin: 0 auto;
   return (
     <>
-      <div className=" max-w-[1500px] sm:px-[53px] lg:px-16  pt-[140px]">
+      <div className=" max-w-[1500px] sm:px-[53px] lg:px-16  pt-[140px] mx-auto">
         <div className={`mx-auto `}>
           <div className="px-6 sm:px-0">
             <h2
@@ -91,7 +20,7 @@ const HomeHero = () => {
               Fluid minds, <br />
               solid digital experiences.
             </h2>
-            <span ref={h3Ref}></span>
+
             <h3
               className={` mt-10 font-light mr-[50px] text-[24px] leading-[32px] text-[#B5B5B5] }`}
             >
